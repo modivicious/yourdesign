383c0da8ae9812bb72b2f0a1242ebca9c1759d33
modivicious <dens.2001@bk.ru>
2021-12-22 03:24:26 +0300

    added recent, stages and reviews sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
-$(function(){
+$(function () {
+  $('.recent__list').slick({
+    rows: 2,
+    slidesPerRow: 3,
+  });
 
+  $('.reviews__list').slick({
+    fade: true,
+    prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><svg width="35" height="60" opacity=".4" fill="#D77E4A"><use href="images/icons/sprite.svg#arrow-left"></use></svg></button >',
+    nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button" style=""><svg width="35" height="60" opacity=".4" fill="#D77E4A"><use href="images/icons/sprite.svg#arrow-right"></use></svg></button >',
+  });
 });
