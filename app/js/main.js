40cf73ae9f4b3498dc94520876671eb5e8612e03
modivicious <dens.2001@bk.ru>
2021-12-30 05:01:23 +0300

    added more effects, fixed some bugs

--- app/js/main.js
+++ app/js/main.js
@@ -42,44 +42,98 @@ $(function () {
 
   $('.search__btn').on('click', function () {
     if (screenWidth > 1200)
-      $('.search__input').animate({ width: 'toggle', paddingLeft: 'toggle', paddingRight: 'toggle' }, 400);
+      $('.search__input').animate({ width: 'toggle', paddingLeft: 'toggle', paddingRight: 'toggle' }, 500);
   });
-});
 
-function toggleMenu() {
-  $(".menu__list").toggleClass("menu__list--active");
-  $(".menu__overlay").toggleClass("menu__overlay--active");
-  $(".menu__btn").toggleClass("menu__btn--active");
-  $("body").toggleClass("hide-overflow");
-}
+  function toggleMenu() {
+    $(".menu__list").toggleClass("menu__list--active");
+    $(".menu__overlay").toggleClass("menu__overlay--active");
+    $(".menu__btn").toggleClass("menu__btn--active");
+    $("body").toggleClass("hide-overflow");
+  }
 
-$(".menu__btn").on("click", () => toggleMenu());
-$(".menu__overlay").on("click", () => toggleMenu());
+  $(".menu__btn").on("click", () => toggleMenu());
+  $(".menu__overlay").on("click", () => toggleMenu());
 
 
-const animItems = document.querySelectorAll(".animation");
+  const animItems = document.querySelectorAll(".animation");
 
-function animateStart(e) {
-  if (e.dataset.anim)
-    e.classList.add(e.dataset.anim);
-  else return;
-}
-
-const imgOptions = {
-  threshold: 0,
-  rootMargin: "4000px 0px -80px 0px",
-};
-
-const animObserver = new IntersectionObserver((entries, animObserver) => {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) return;
-    else {
-      animateStart(entry.target);
-      animObserver.unobserve(entry.target);
+  function someAnimation(e, anim) {
+    var items = e.querySelectorAll('.some-anim');
+    switch (anim) {
+      // case 'hover-anim':
+      //   var time = 100;
+      //   for (let i = 0; i < items.length; i++) {
+      //     setTimeout(() => {
+      //       items[i].classList.add('hoverAnim');
+      //       setTimeout(() => items[i].classList.remove('hoverAnim'), time);
+      //     }, (i + 1) * 400);
+      //     time += 100;
+      //   }
+      //   break;
+      case 'list-anim':
+        for (let i = 0; i < items.length; i++) {
+          setTimeout(() => {
+            items[i].classList.add('list-anim');
+          }, (i + 1) * 250);
+        }
+        break;
     }
-  });
-}, imgOptions);
+  }
 
-animItems.forEach((anim) => {
-  animObserver.observe(anim);
+  function animateStart(e) {
+    if (e.dataset.anim) {
+      switch (e.dataset.anim) {
+        case 'list-anim':
+          someAnimation(e, 'list-anim');
+          break;
+        // case 'hoverAnim':
+        //   someAnimation(e, 'hover-anim');
+        //   break;
+        default:
+          e.classList.add(e.dataset.anim);
+      }
+    }
+    if (e.dataset.extraanim) e.classList.add(e.dataset.extraanim);
+
+  }
+
+  const animOptions = {
+    threshold: 0,
+    rootMargin: "10000px 0px -60px 0px",
+  };
+
+  const animObserver = new IntersectionObserver((entries, animObserver) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) return;
+      else {
+        animateStart(entry.target);
+        animObserver.unobserve(entry.target);
+      }
+    });
+  }, animOptions);
+
+  animItems.forEach((anim) => {
+    animObserver.observe(anim);
+  });
+
+
+  const scrollImages = document.querySelectorAll('.scroll-image');
+
+  if (scrollImages.length > 0) {
+    window.addEventListener('scroll', () => {
+      scrollImages.forEach((image) => {
+        const imageHeight = image.offsetHeight;
+        const imageOffset = image.offsetTop;
+        var imageX = imageOffset - imageHeight;
+
+        if (scrollY > imageX) {
+          var imageScrolled = scrollY - imageX;
+          var backgroundPosition = imageScrolled * 0.02;
+          image.style.backgroundPosition = `${backgroundPosition}%`;
+        }
+      });
+    });
+  }
+
 });
